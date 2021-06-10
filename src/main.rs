0816fb9b81b664c5a4f2c623787d2052775c77ee
Parker McMullin <parker.mcmullin01@gmail.com>
2021-06-10 09:24:51 -0700

    Update to Sycamore. Add counter and text binding examples.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,49 @@
-use maple_core::prelude::*;
+use sycamore::prelude::*;
 
 fn main() {
-    let root = template! {
-        p {
-            "Hello World!"
-        }
-    };
+    render(|| template! { App() });
+}
 
-    render(|| root);
+#[component(App<G>)]
+fn app() -> TemplateResult<G> {
+    template! {
+        Counter()
+
+        TextBinding()
+    }
+}
+
+#[component(Counter<G>)]
+fn counter() -> TemplateResult<G> {
+    let counter = Signal::new(0);
+
+    let decrement = cloned!((counter) => move |_| counter.set(*counter.get() - 1));
+    let increment = cloned!((counter) => move |_| counter.set(*counter.get() + 1));
+
+    template! {
+        div(class="counter") {
+            button(class="decrement", on:click=decrement) { "-" }
+            (counter.get())
+            button(class="increment", on:click=increment) { "+" }
+        }
+    }
+}
+
+#[component(TextBinding<G>)]
+fn text_binding() -> TemplateResult<G> {
+    let value = Signal::new(String::new());
+
+    let uppercase = create_memo(cloned!((value) => move || {
+        return value.get().to_uppercase();
+    }));
+
+    template! {
+        div(class="text-binding") {
+            input(type="text", bind:value=value)
+
+            div(class="uppercase-text") {
+                (uppercase.get())
+            }
+        }
+    }
 }
